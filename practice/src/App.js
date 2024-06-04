3c67218ed492f9a2574b7425db56476c70deac63
Yoosun-Chang <12201321@inha.edu>
2024-06-04 17:18:30 +0900

    Feat: counter 만들기 실습 완료

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const [dogs, setDogs] = useState(["말티즈"]);
-
-  function onClickHandler() {
-		// spread operator(전개 연산자)를 이용해서 dogs를 복사합니다. 
-	  // 그리고 나서 항목을 추가합니다.
-    setDogs([...dogs, "시고르자브르종"]);
-  }
-
-  console.log(dogs);
+  const [number, setNumber] = useState(0);
   return (
     <div>
-      <button onClick={onClickHandler}>버튼</button>
+      <div>{number}</div>
+      <button
+        onClick={() => {
+          setNumber(number + 1);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setNumber(number - 1);
+        }}
+      >
+        -
+      </button>
     </div>
   );
 }
