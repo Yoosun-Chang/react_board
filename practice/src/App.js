a7234b2ffaf4ae3343a7da5b1dbbc3879540ec33
Yoosun-Chang <12201321@inha.edu>
2024-06-04 17:27:35 +0900

    Feat: css 분리 및 map을 이용해서 구현해보기

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,29 +1,19 @@
 import React from "react";
+import './App.css'; 
 
 const App = () => {
-  const style = {
-    padding: "100px",
-    display: "flex",
-    gap: "12px",
-  };
-
-  const squareStyle = {
-    width: "100px",
-    height: "100px",
-    border: "1px solid green",
-    borderRadius: "10px",
-		display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  };
+  
+  const vegetables = ["감자", "고구마", "오이", "가지", "옥수수"];
 
   return (
-    <div style={style}>
-      <div style={squareStyle}>감자</div>
-      <div style={squareStyle}>고구마</div>
-      <div style={squareStyle}>오이</div>
-      <div style={squareStyle}>가지</div>
-      <div style={squareStyle}>옥수수</div>
+    <div className="app-style">
+      {vegetables.map((vegetableName) => {
+        return (
+          <div className="square-style" key={vegetableName}>
+            {vegetableName}
+          </div>
+        );
+      })}
     </div>
   );
 };
