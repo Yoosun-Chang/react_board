aa059a9e5adaec303d2e6d319a4f7d9c59df7aa2
Yoosun-Chang <12201321@inha.edu>
2024-06-04 17:50:22 +0900

    Feat: 조금 더 복잡한 데이터 다뤄보기

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import './App.css';
+import React,{useState} from 'react';
+import './App.css'; 
+
+function CustomButton(props) {
+  const {color, onClick, children} = props
+  
+    if (color)
+      return (
+        <button
+          style={{ background: color, color: "white" }}
+          onClick={onClick}
+        >
+          {children}
+        </button>
+      );
+  
+    return <button onClick={onClick}>{props.children}</button>;
+  }
+
 
 function User(props) {
   return (
-    <div className="square-style">  {/* Apply square-style class here */}
-      {props.user.age}살 - {props.user.name}
+    <div className="square-style">
+      <div>{props.user.age}살 - </div>
+      <div>{props.user.name}</div>
+      <CustomButton color="red" onClick={() => props.handleDelete(props.user.id)}>
+        삭제하기
+      </CustomButton>
     </div>
   );
 }
 
 const App = () => {
-  const users = [
+  const [users, setUsers] = useState([
     { id: 1, age: 30, name: '송중기' },
     { id: 2, age: 24, name: '송강' },
     { id: 3, age: 21, name: '김유정' },
     { id: 4, age: 29, name: '구교환' },
-  ];
+  ]);
+  const [name, setName] = useState('');
+  const addUserHandler = () => {
+    const newUser = {
+      id: users.length + 1,
+      age: 30,
+      name: name,
+    };
+
+    setUsers([...users, newUser]);
+  };
+	const deleteUserHandler = (id) => {
+    const newUserList = users.filter((user) => user.id !== id);
+    setUsers(newUserList);
+  };
   return (
     <div className="app-container">
+      <input
+        placeholder="이름을 입력해주세요"
+        value={name}
+        onChange={(e) => setName(e.target.value)} 
+      />
       {users.map((user) => {
-        return <User className="square-style" user={user} key={user.id} />; 
+        return <User user={user} key={user.id} handleDelete={deleteUserHandler}/>;
       })}
+      <CustomButton color="green" onClick={addUserHandler}>추가하기</CustomButton>
     </div>
   );
 };
