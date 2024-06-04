ce8521ef75159018c47f4e45c5b8ec49c64b6e72
Yoosun-Chang <12201321@inha.edu>
2024-06-04 17:30:23 +0900

    Feat: 객체가 담긴 배열 다뤄보기

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,18 +1,25 @@
-import React from "react";
-import './App.css'; 
+import React from 'react';
+import './App.css';
+
+function User(props) {
+  return (
+    <div className="square-style">  {/* Apply square-style class here */}
+      {props.user.age}살 - {props.user.name}
+    </div>
+  );
+}
 
 const App = () => {
-  
-  const vegetables = ["감자", "고구마", "오이", "가지", "옥수수"];
-
+  const users = [
+    { id: 1, age: 30, name: '송중기' },
+    { id: 2, age: 24, name: '송강' },
+    { id: 3, age: 21, name: '김유정' },
+    { id: 4, age: 29, name: '구교환' },
+  ];
   return (
-    <div className="app-style">
-      {vegetables.map((vegetableName) => {
-        return (
-          <div className="square-style" key={vegetableName}>
-            {vegetableName}
-          </div>
-        );
+    <div className="app-container">
+      {users.map((user) => {
+        return <User className="square-style" user={user} key={user.id} />; 
       })}
     </div>
   );
