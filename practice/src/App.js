fe55f104cbbf5245ebbf02ed556b57b39c04abc6
Yoosun-Chang <12201321@inha.edu>
2024-06-04 20:52:52 +0900

    Feat: useMemo 사용해보기2

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,24 +1,52 @@
-import "./App.css";
-import HeavyComponent from "./components/HeavyComponent";
+import React, { useEffect, useState, useMemo } from "react";
 
-function App() {
-  const navStyleObj = {
-    backgroundColor: "yellow",
-    marginBottom: "30px",
-  };
+function ObjectComponent() {
+  const [isAlive, setIsAlive] = useState(true);
+  const [uselessCount, setUselessCount] = useState(0);
 
-  const footerStyleObj = {
-    backgroundColor: "green",
-    marginTop: "30px",
-  };
+  const me = useMemo(() => {
+    return {
+      name: "Ted Chang",
+      age: 21,
+      isAlive: isAlive ? "생존" : "사망",
+    };
+  }, [isAlive]);
+
+  useEffect(() => {
+    console.log("생존여부가 바뀔 때만 호출해주세요!");
+  }, [me]);
 
   return (
     <>
-      <nav style={navStyleObj}>네비게이션 바</nav>
-      <HeavyComponent />
-      <footer style={footerStyleObj}>푸터 영역이에요</footer>
+      <div>
+        내 이름은 {me.name}이구, 나이는 {me.age}야!
+      </div>
+      <br />
+      <div>
+        <button
+          onClick={() => {
+            setIsAlive(!isAlive);
+          }}
+        >
+          누르면 살았다가 죽었다가 해요
+        </button>
+        <br />
+        생존여부 : {me.isAlive}
+      </div>
+      <hr />
+      필요없는 숫자 영역이에요!
+      <br />
+      {uselessCount}
+      <br />
+      <button
+        onClick={() => {
+          setUselessCount(uselessCount + 1);
+        }}
+      >
+        누르면 숫자가 올라가요
+      </button>
     </>
   );
 }
 
-export default App;
+export default ObjectComponent;
