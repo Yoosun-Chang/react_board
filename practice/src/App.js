c9c080e9305bc2d112c61f0a33f28db9aa774dcd
Yoosun-Chang <12201321@inha.edu>
2024-06-04 20:17:50 +0900

    Feat: 조건부 스타일링 구현하기 리팩토링

--- practice/src/App.js
+++ practice/src/App.js
@@ -3,23 +3,40 @@
 import React from "react";
 import styled from "styled-components";
 
-// 1. styled-components를 만들었습니다.
+const StContainer = styled.div`
+  display: flex;
+`;
+
 const StBox = styled.div`
   width: 100px;
   height: 100px;
-  border: 1px solid ${(props) => props.borderColor}; // 4.부모 컴포넌트에서 보낸 props를 받아 사용합니다. 
+  border: 1px solid ${(props) => props.borderColor};
   margin: 20px;
 `;
+// 박스의 색을 배열에 담습니다.
+const boxList = ["red", "green", "blue"];
 
+// 색을 넣으면, 이름을 반환해주는 함수를 만듭니다.
+const getBoxName = (color) => {
+  switch (color) {
+    case "red":
+      return "빨간 박스";
+    case "green":
+      return "초록 박스";
+    case "blue":
+      return "파란 박스";
+    default:
+      return "검정 박스";
+   }
+};
 const App = () => {
   return (
-    <div>
-			{/* 2. 그리고 위에서 만든 styled-components를 사용했습니다. */}
-			{/* 3. 그리고 props를 통해 borderColor라는 값을 전달했습니다. */}
-      <StBox borderColor="red">빨간 박스</StBox>
-      <StBox borderColor="green">초록 박스</StBox>
-      <StBox borderColor="blue">파랑 박스</StBox>
-    </div>
+    <StContainer>
+			{/* map을 이용해서 StBox를 반복하여 화면에 그립니다. */}
+      {boxList.map((box) => (
+        <StBox borderColor={box}>{getBoxName(box)}</StBox>
+      ))}
+    </StContainer>
   );
 };
 
