94aea092faa199760c6ddcb6f0c56b38256b74e2
Yoosun-Chang <12201321@inha.edu>
2024-06-04 20:44:00 +0900

    Feat: memo 사용전 리렌더링 확인

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,41 +1,38 @@
-import { useEffect, useRef, useState } from "react";
-import "./App.css";
+import React, { useState } from "react";
+import Box1 from "./components/Box1";
+import Box2 from "./components/Box2";
+import Box3 from "./components/Box3";
+
+const boxesStyle = {
+  display: "flex",
+  marginTop: "10px",
+};
 
 function App() {
-  const idRef = useRef("");
-  const pwRef = useRef("");
+  console.log("App 컴포넌트가 렌더링되었습니다!");
 
-  const [id, setId] = useState("");
+  const [count, setCount] = useState(0);
 
-  const onIdChangeHandler = (event) => {
-    setId(event.target.value);
+  // 1을 증가시키는 함수
+  const onPlusButtonClickHandler = () => {
+    setCount(count + 1);
   };
 
-  // 렌더링이 될 때
-  useEffect(() => {
-    idRef.current.focus();
-  }, []);
-
-  // 왜 useEffect 안에 놓았을까요?
-  useEffect(() => {
-    if (id.length >= 10) {
-      pwRef.current.focus();
-    }
-  }, [id]);
+  // 1을 감소시키는 함수
+  const onMinusButtonClickHandler = () => {
+    setCount(count - 1);
+  };
 
   return (
     <>
-      <div>
-        아이디 :
-        <input
-          type="text"
-          ref={idRef}
-          value={id}
-          onChange={onIdChangeHandler}
-        />
-      </div>
-      <div>
-        비밀번호 : <input type="password" ref={pwRef} />
+      <h3>카운트 예제입니다!</h3>
+      <p>현재 카운트 : {count}</p>
+      <button onClick={onPlusButtonClickHandler}>+</button>
+      <button onClick={onMinusButtonClickHandler}>-</button>
+      <div style={boxesStyle}>
+        <Box1 />
+        <Box2 />
+        <Box3 />
       </div>
     </>
   );
