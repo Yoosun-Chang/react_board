3728f1ab9f868e882e254b6c41242cdd70891d03
Yoosun-Chang <12201321@inha.edu>
2024-06-04 16:36:06 +0900

    Feat: State 변경하기

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
 
 function Child(props) {
-  return <div>{props.grandFatherName}</div>;
+  return (
+    <div>
+      <button
+        onClick={() => {
+          props.setName("박할아");
+        }}
+      >
+        할아버지 이름 바꾸기
+      </button>
+      <div>{props.grandFatherName}</div>
+    </div>
+  );
 }
 
 function Mother(props) {
-  return <Child grandFatherName={props.grandFatherName} />;
+  return (
+    <Child grandFatherName={props.grandFatherName} setName={props.setName} />
+  );
 }
 
 function GrandFather() {
-  const [name, setName] = useState("김할아"); // state를 생성
-  return <Mother grandFatherName={name} />;
+  const [name, setName] = useState("김할아");
+  return <Mother grandFatherName={name} setName={setName} />;
 }
 
 function App() {
