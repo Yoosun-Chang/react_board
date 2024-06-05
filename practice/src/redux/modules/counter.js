4d9dd06897cbc5da78139c6860c29aeb176f1018
Yoosun-Chang <12201321@inha.edu>
2024-06-05 12:12:51 +0900

    Feat: -n  기능을 구현

--- practice/src/redux/modules/counter.js
+++ practice/src/redux/modules/counter.js
@@ -1,45 +1,40 @@
-// 추가된 코드 👇 - 액션 value를 상수들로 만들어 줍니다. 보통 이렇게 한곳에 모여있습니다.
-const PLUS_ONE = "PLUS_ONE";
-const MINUS_ONE = "MINUS_ONE";
 const ADD_NUMBER = "ADD_NUMBER";
-
-// 추가된 코드 👇 - Action Creator를 만들어 줍니다. 
-export const plusOne = () => {
-  return {
-    type: PLUS_ONE,
-  };
-};
-
-export const minusOne = () => {
-  return {
-    type: MINUS_ONE,
-  };
-};
+const MINUS_NUMBER = "MINUS_NUMBER";
 
 export const addNumber = (payload) => {
-    return {
-      type: ADD_NUMBER,
-      payload,
-    };
+  return {
+    type: ADD_NUMBER,
+    payload,
   };
+};
 
-// 초기 상태값
+export const minusNumber = (payload) => {
+  return {
+    type: MINUS_NUMBER,
+    payload,
+  };
+};
+
+// Initial State
 const initialState = {
   number: 0,
 };
 
-// 리듀서
+// Reducer
 const counter = (state = initialState, action) => {
-    switch (action.type) {
-      case ADD_NUMBER:
-        return {
-          // state.number (기존의 nubmer)에 action.paylaod(유저가 더하길 원하는 값)을 더한다.
-          number: state.number + action.payload,
-        };
-      default:
-        return state;
-    }
-  };
+  switch (action.type) {
+    case ADD_NUMBER:
+      return {
+        number: state.number + action.payload,
+      };
+    case MINUS_NUMBER:
+      return {
+        number: state.number - action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
-
+// export default reducer
 export default counter;
