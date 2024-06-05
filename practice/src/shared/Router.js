7e50c652ce591cdf7ad50218e8d2c8ba518b9d13
Yoosun-Chang <12201321@inha.edu>
2024-06-05 12:59:16 +0900

    Feat: children 용도

--- practice/src/shared/Router.js
+++ practice/src/shared/Router.js
@@ -1,19 +1,22 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
-import Works from "../pages/Works";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
+import Works from '../pages/Works';
+import Layout from './Layout';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="works" element={<Works />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="works" element={<Works />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
