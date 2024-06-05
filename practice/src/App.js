1371ff44d100b5be2bbe76bdeb87f08f80b834a6
Yoosun-Chang <12201321@inha.edu>
2024-06-05 11:51:01 +0900

    Feat: 카운터 프로그램 만들기2

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,11 +1,38 @@
 import React from "react";
-import { useSelector } from "react-redux"; // import 해주세요.
+import { useDispatch, useSelector } from "react-redux"; // import 해주세요.
 
 const App = () => {
-  const counterStore = useSelector((state) => state); // 추가해주세요.
-  console.log(counterStore); // 스토어를 조회해볼까요?
+  const dispatch = useDispatch(); // dispatch 생성
 
-  return <div></div>;
-}
+  const number = useSelector((state) => state.counter.number); 
+
+  console.log(number);
+
+  return (
+    <div>
+      {number}
+      <button
+				// 이벤트 핸들러 추가
+        onClick={() => {
+					// 마우스를 클릭했을 때 dispatch가 실행되고, ()안에 있는 액션객체가 리듀서로 전달된다.
+          dispatch({ type: "PLUS_ONE" }); 
+        }}
+      >
+				+ 1
+      </button>
+
+      <button
+				// 이벤트 핸들러 추가
+        onClick={() => {
+					// 마우스를 클릭했을 때 dispatch가 실행되고, ()안에 있는 액션객체가 리듀서로 전달된다.
+          dispatch({ type: "MINUS_ONE" }); 
+        }}
+      >
+				- 1
+      </button>
+
+    </div>
+  );
+};
 
 export default App;
--- practice/src/redux/modules/counter.js
+++ practice/src/redux/modules/counter.js
@@ -4,7 +4,24 @@ const initialState = {
   
   // 리듀서
   const counter = (state = initialState, action) => {
+    console.log(action);
     switch (action.type) {
+          // PLUS_ONE이라는 case를 추가한다.
+          // 여기서 말하는 case란, action.type을 의미한다.
+          // dispatch로부터 전달받은 action의 type이 "PLUS_ONE" 일 때
+          // 아래 return 절이 실행된다. 
+      case "PLUS_ONE":
+        return {
+          // 기존 state에 있던 number에 +1을 더한다.
+          number: state.number + 1,
+        };
+
+      case "MINUS_ONE":
+            return {
+              // 기존 state에 있던 number에 +1을 더한다.
+              number: state.number - 1,
+            };
+  
       default:
         return state;
     }
