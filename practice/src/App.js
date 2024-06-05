da6a3df65a841bed7b3c48be9381b9f2cf9fdb77
Yoosun-Chang <12201321@inha.edu>
2024-06-05 11:40:45 +0900

    Feat: redux 설정 + 카운터 프로그램 만들기

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,52 +1,11 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React from "react";
+import { useSelector } from "react-redux"; // import 해주세요.
 
-function ObjectComponent() {
-  const [isAlive, setIsAlive] = useState(true);
-  const [uselessCount, setUselessCount] = useState(0);
+const App = () => {
+  const counterStore = useSelector((state) => state); // 추가해주세요.
+  console.log(counterStore); // 스토어를 조회해볼까요?
 
-  const me = useMemo(() => {
-    return {
-      name: "Ted Chang",
-      age: 21,
-      isAlive: isAlive ? "생존" : "사망",
-    };
-  }, [isAlive]);
-
-  useEffect(() => {
-    console.log("생존여부가 바뀔 때만 호출해주세요!");
-  }, [me]);
-
-  return (
-    <>
-      <div>
-        내 이름은 {me.name}이구, 나이는 {me.age}야!
-      </div>
-      <br />
-      <div>
-        <button
-          onClick={() => {
-            setIsAlive(!isAlive);
-          }}
-        >
-          누르면 살았다가 죽었다가 해요
-        </button>
-        <br />
-        생존여부 : {me.isAlive}
-      </div>
-      <hr />
-      필요없는 숫자 영역이에요!
-      <br />
-      {uselessCount}
-      <br />
-      <button
-        onClick={() => {
-          setUselessCount(uselessCount + 1);
-        }}
-      >
-        누르면 숫자가 올라가요
-      </button>
-    </>
-  );
+  return <div></div>;
 }
 
-export default ObjectComponent;
+export default App;
