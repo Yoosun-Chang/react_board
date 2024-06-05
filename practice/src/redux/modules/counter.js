cc0351b5bb4d3da0d4a912c60a2f55d4550b547c
Yoosun-Chang <12201321@inha.edu>
2024-06-05 12:10:00 +0900

    Feat: payload를 이용해서 기능 구현하기

--- practice/src/redux/modules/counter.js
+++ practice/src/redux/modules/counter.js
@@ -1,7 +1,7 @@
 // 추가된 코드 👇 - 액션 value를 상수들로 만들어 줍니다. 보통 이렇게 한곳에 모여있습니다.
 const PLUS_ONE = "PLUS_ONE";
 const MINUS_ONE = "MINUS_ONE";
-
+const ADD_NUMBER = "ADD_NUMBER";
 
 // 추가된 코드 👇 - Action Creator를 만들어 줍니다. 
 export const plusOne = () => {
@@ -16,6 +16,12 @@ export const minusOne = () => {
   };
 };
 
+export const addNumber = (payload) => {
+    return {
+      type: ADD_NUMBER,
+      payload,
+    };
+  };
 
 // 초기 상태값
 const initialState = {
@@ -24,19 +30,16 @@ const initialState = {
 
 // 리듀서
 const counter = (state = initialState, action) => {
-  switch (action.type) {
-    case PLUS_ONE: // case에서도 문자열이 아닌, 위에서 선언한 상수를 넣어줍니다. 
-      return {
-        number: state.number + 1,
-      };
-    case MINUS_ONE: // case에서도 문자열이 아닌, 위에서 선언한 상수를 넣어줍니다. 
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-};
+    switch (action.type) {
+      case ADD_NUMBER:
+        return {
+          // state.number (기존의 nubmer)에 action.paylaod(유저가 더하길 원하는 값)을 더한다.
+          number: state.number + action.payload,
+        };
+      default:
+        return state;
+    }
+  };
 
 
 export default counter;
