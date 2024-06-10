5d09d65b80929a8081c99ae615db0c33f5577800
Yoosun-Chang <12201321@inha.edu>
2024-06-10 10:05:37 +0900

    Feat: 배포를 위해 practice 폴더 삭제

--- practice/src/components/Layout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Layout = () => {
-  return (
-    <div>이건 모든 페이지에서 보이는 헤더 입니다.</div>
-  )
-}
-
-export default Layout
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '../src/App.css'
+import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import styled from "styled-components";
 import MainPage from './component/page/MainPage';
